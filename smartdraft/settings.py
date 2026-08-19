f4b23d25c09c8f8698f6fa80fe2dd15c757000a7
Sagar Raturi <sagarraturi85516@gmail.com>
2026-08-19 12:58:31 +0530

    Added collectstatic

--- smartdraft/settings.py
+++ smartdraft/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -206,3 +208,5 @@ ACCOUNT_UNIQUE_EMAIL = True
 
 # related to deployment
 ACCOUNT_USERNAME_BLACKLIST = ['admin', 'users', 'post', 'profile', 'theboss']
+
+INTERNAL_IPS = ('127.0.0.1', 'localhost:8000')
